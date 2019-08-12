65953943023579b15545fcad5eb1dc79136adf12
adriangrahl <adrian.grahl@gmail.com>
2019-08-12 01:44:15 -0300

    Backend final touch - socket io - its a match feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-mongoose.connect('SECRETKEY', {
-    useNewUrlParser: true    
-})
+const connectedUsers = {};
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+  console.log("new connection, client id: ", socket.id, user);
+  connectedUsers[user] = socket.id;
+});
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+mongoose.connect("SECRETKEY", {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
