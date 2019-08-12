cdc704a5eea8c0cb25068116770255a25b91f250
adriangrahl <adrian.grahl@gmail.com>
2019-08-12 01:45:12 -0300

    its a match feature - socket io - frontend

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,77 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import './Main.css'
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
-import like from '../assets/like.svg';
-import dislike from '../assets/dislike.svg';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./Main.css";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import itsamatch from "../assets/itsamatch.png";
 
-import api from '../services/api';
+import api from "../services/api";
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
-    useEffect(() => {
-        async function loadUsers() {
-            const {data} = await api.get(`/devs/`, {
-                headers: { 
-                    user: match.params.id,
-                }
-            })
-            setUsers(data);
+  useEffect(() => {
+    async function loadUsers() {
+      const { data } = await api.get(`/devs/`, {
+        headers: {
+          user: match.params.id
         }
-        loadUsers();
-    }, [match.params.id]);
-
-    async function handleLike(id) {
-        await api.post(`/devs/${id}/likes`, null, {
-            headers: {
-                user: match.params.id,
-            }
-        })
-
-        setUsers(users.filter(user => user._id !== id));
+      });
+      setUsers(data);
     }
+    loadUsers();
+  }, [match.params.id]);
 
-    async function handleDislike(id) {
-        await api.post(`/devs/${id}/dislikes`, null, {
-            headers: {
-                user: match.params.id,
-            }
-        })
+  useEffect(() => {
+    const socket = io("http://localhost:3333", {
+      query: { user: match.params.id }
+    });
 
-        setUsers(users.filter(user => user._id !== id));
-    }
+    socket.on("match", dev => setMatchDev(dev));
+  }, [match.params.id]);
 
-    return (
-        <div className="main-container">
-            <Link to="/">
-                <img src={logo} alt="Tindev" />
-            </Link>
-            { users.length > 0 ? (
-                <ul>
-                    {users.map(user => (
-                        <li key={user._id}>
-                            <img src={user.avatar} alt={user.avatar}/>
-                            <footer>
-                                <strong>{user.name}</strong>
-                                <p>{user.bio}</p>
-                            </footer>
-    
-                            <div className="buttons">
-                                <button type="button" onClick={() => handleDislike(user._id)}>
-                                    <img src={dislike} alt="Dislike" />
-                                </button>
-                                <button type="button" onClick={() => handleLike(user._id)}>
-                                    <img src={like} alt="Like" />
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <div className="empty">Empty :(</div>
-            )}
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img id="avatar" src={user.avatar} alt={user.avatar} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Empty :(</div>
+      )}
+
+      {matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="its a match" />
+
+          <img className="avatar" src={matchDev.avatar} alt="avatar" />
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+
+          <button type="button" onClick={() => setMatchDev(null)}>
+            Close
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
